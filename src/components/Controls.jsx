3e7091c78d1a05a77655377bf851840b63e54f0d
KishoM2Mac <kisokumar886@gmail.com>
2023-12-20 20:46:10 +0000

    refactor: header component

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,18 +1,9 @@
-import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Controls() {
   return (
     <>
-      <div className="fixed top-0 w-full shadow-custom">
-        <header className="scale-[85%] mt-4 flex w-full items-center justify-center px-4 mt-0  py-2 mb-1">
-          <div className="flex text-sm text-white space-x-6 md:mt-0">
-            <h1 className="transition ease-in-out hover:scale-110 duration-300 w-max bg-gradient-to-r from-indigo-900 via-indigo-500 to-cyan-400 font-Audiowide text-lg text-center flex items-center rounded-full border-[3px] );">
-              &nbsp;&nbsp;&nbsp;Galaxy Quest&nbsp;&nbsp;&nbsp;
-            </h1>
-          </div>
-        </header>
-      </div>
+      <Header />
 
       <div className="flex flex-col border-2 rounded-2xl p-8 w-fit bg-black bg-opacity-30	 text-white">
         <div className="w-full flex justify-center">
@@ -39,5 +30,19 @@ export default function Controls() {
         <Footer />
       </div>
     </>
+  );
+}
+
+function Header() {
+  return (
+    <div className="fixed top-0 w-full shadow-custom">
+      <header className="scale-[85%] mt-1 flex w-full items-center justify-center px-4 py-2 mb-1">
+        <div className="flex text-sm text-white space-x-6 md:mt-0">
+          <h1 className="transition ease-in-out hover:scale-110 duration-300 w-max bg-gradient-to-r from-indigo-900 via-indigo-500 to-cyan-400 font-Audiowide text-lg text-center flex items-center rounded-full border-[3px] );">
+            &nbsp;&nbsp;&nbsp;Galaxy Quest&nbsp;&nbsp;&nbsp;
+          </h1>
+        </div>
+      </header>
+    </div>
   );
 }
