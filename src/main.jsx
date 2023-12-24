ccf19357c8118df263c4a9b6227df33741821371
KishoM2Mac <kisokumar886@gmail.com>
2023-12-24 22:26:05 +0000

    fix: deploy issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
 import "./index.css";
 
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="galaxy-quest">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
